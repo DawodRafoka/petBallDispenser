565cb682cec68ad5537cfe0c85319d2197d2d7d5
DawodRafoka <dawodrafoka@gmail.com>
2021-10-20 19:27:26 -0700

    Added sticky footer

--- js/scripts.js
+++ js/scripts.js
@@ -25,6 +25,33 @@
 // });
 // $('.formContainer').find( 'textarea' ).keydown();
 
+// var lastScroll = 0;
+// var footer = document.getElementById("footer");
+// window.addEventListener("scroll", function(){
+//     var scrollDown = window.scrollY || document.documentElement.scrollDown;
+//     if(scrollDown > lastScroll)
+//     {
+//         footer.style.height = "-4rem";
+//     }
+//     else
+//     {
+//         footer.style.height = "0";
+//     }
+//     lastScroll = scrollDown;
+// })
+
+
+// window.onscroll = function(ev) {
+//     if((window.innerHeight + window.scrollY) >= document.body.offsetHeight)
+//     {
+//         document.getElementsByClassName("footer").style.height = "-4rem";
+        
+//         console.log("innerHeight =" + window.innerHeight);
+//         console.log("scrollY = " +window.scrollY);
+
+//     }
+// };
+
 function validateForm() {
     if((document.forms["suggestionsForm"]["fname"].value == "") ||
        (document.forms["suggestionsForm"]["comments"].value == ""))
@@ -47,6 +74,7 @@ function showForm() {
     //     x.style.display = "none";
     // }
 }
+
 function closeForm() {
     // document.getElementById("popupForm").style.display = "none";
     var blur = document.getElementById("bg");
@@ -55,3 +83,65 @@ function closeForm() {
     // popupForm.classList.toggle("active");
     blur.classList.toggle("active");
 }
+
+//Referenced from
+//https://codepen.io/whins/pen/mPBREq
+// var scrolled;
+// var lastscrollTop = 0;
+// var deltaValue = 5;
+// var footerHeight = $('footer').outerHeight();
+
+// $(window).scroll(function(event){
+//     scrolled = true;
+// });
+
+// setInterval(funciton() {
+//     if(scrolled)
+//     {
+//         hasScrolled();
+//         scrolled = false;
+//     }
+// }, 250);
+
+// function hasScrolled() {
+//     var st = $(this).scrollTop();
+
+//     //effect starts after deltaValue
+//     if(Math.abs(lastscrollTop - st) <= deltaValue)
+//         return;
+    
+//     if(st > lastscrollTop && st > footerHeight) {
+//         $('footer').removeClass()
+//     }
+// }
+
+// $(document).on('scrollstart scrolled', 'body', function(event) {
+//     var $page = $(this);
+//     if(event.type == 'scrollstart')
+//         $page.addClass('is_scrolling');
+//     else
+//         $page.removeClass('is_scrolling');
+// });
+// console.log("Hello");
+// $(window).scroll(function() {
+//     if($(this).scrollTop() > 0) 
+//     {
+//         $('.footer').fadeOut();
+//         console.log(scrollTop());
+//     }
+//     else
+//     {
+//         $('.footer').fadeIn();
+//     }
+// });
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("topNav").style.top = "0";
+//   } else {
+//     document.getElementById("topNav").style.top = "-50px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
