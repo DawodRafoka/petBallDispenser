0464801eecc221964353e47de77becc4e41d4a74
DawodRafoka <dawodrafoka@gmail.com>
2021-10-31 15:45:54 -0700

    Added Code segment in firmware

--- js/scripts.js
+++ js/scripts.js
@@ -145,3 +145,28 @@ function closeForm() {
 //   }
 //   prevScrollpos = currentScrollPos;
 // }
+
+
+// https://www.prowaretech.com/Computer/JavaScript/AddLineNumbersToPre
+function addLineClass (pre) {
+    var lines = pre.innerText.split("\n"); // can use innerHTML also
+    // console.log(pre.childNodes.length);
+    while(pre.childNodes.length > 0) {
+        pre.removeChild(pre.childNodes[0]);
+    }
+    for(var i = 0; i < lines.length; i++) {
+        var span = document.createElement("span");
+        span.className = "line";
+        span.innerText = lines[i]; // can use innerHTML also
+        pre.appendChild(span);
+        pre.appendChild(document.createTextNode("\n"));
+    }
+}
+// window.addEventListener("load", function () {
+function showLines() {
+    var pres = document.getElementsByTagName("pre");
+    for (var i = 0; i < pres.length; i++) {
+        addLineClass(pres[i]);
+    }
+}
+// }, false);
