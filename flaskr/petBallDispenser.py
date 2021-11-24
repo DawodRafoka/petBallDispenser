886005e5d5f599e42ecda8f61fd6599879aa0a93
Bunnell1101 <67916554+Bunnell1101@users.noreply.github.com>
2021-11-23 22:18:30 -0800

    Working Downloads with tracking.

--- flaskr/petBallDispenser.py
+++ flaskr/petBallDispenser.py
@@ -1,13 +1,13 @@
-from logging import Manager
-from flask import Flask, render_template, request, redirect, url_for,session, flash
+from flask import Flask, render_template, send_from_directory, abort, redirect, url_for,session, flash
 from forms import ContactForm
 import pyodbc
-import pandas as pd
+from werkzeug.utils import safe_join
 from datetime import timedelta
-
+ 
 app = Flask(__name__)
 app.secret_key = 'db216e29fad21ff702244b6c0de749bf8a038b606daf19402e20d13899b2c002'    
-app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)
+app.config['PERMANENT_SESSION_LIFETIME'] =  timedelta(minutes=30)   
+app.config['Parts'] =  "../flaskr/static/Parts"
 conn = None
 try:
     conn = pyodbc.connect('Driver={SQL Server};'
@@ -23,15 +23,15 @@ cursor = conn.cursor()
 def StartSession():
     cursor.execute('SELECT * FROM Sessions')
     temp = max(cursor)
-    print(temp[0])
+    # print(temp[0])
     session['SessionId'] = temp[0] + 1
     query = """INSERT INTO Sessions (SessionId) VALUES (?)"""
     args = (session['SessionId'])
     cursor.execute(query, args)
     cursor.execute('SELECT * FROM Sessions')
-    for row in cursor:
-        print(row)
-    print("Session added")
+    # for row in cursor:
+    #     print(row)
+    # print("Session added")
     return redirect(url_for('index'))
     
 
@@ -43,10 +43,10 @@ def index():
         args = (session['SessionId'],form.firstName.data,form.lastName.data,form.email.data,form.comment.data)
         cursor.execute(query, args).commit()
         flash("Message Received", "success")
-        print("\nData received. ")
+        # print("\nData received. ")
     return render_template("index.html", form=form)
 
-@app.route("/Mechanical", methods=['get', 'post'])
+@app.route("/Mechanical", methods=['get'])
 def mechanical():
     form = ContactForm()
     if form.validate_on_submit():
@@ -54,7 +54,7 @@ def mechanical():
         args = (session['SessionId'],form.firstName.data,form.lastName.data,form.email.data,form.comment.data)
         cursor.execute(query, args).commit()
         flash("Message Received", "success")
-        print("\nData received. ")
+        # print("\nData received. ")
     return render_template("mechanical.html", form=form)
 
 @app.route("/Electrical", methods=['get', 'post'])
@@ -65,7 +65,7 @@ def electrical():
         args = (session['SessionId'],form.firstName.data,form.lastName.data,form.email.data,form.comment.data)
         cursor.execute(query, args).commit()
         flash("Message Received", "success")
-        print("\nData received. ")
+        # print("\nData received. ")
     return render_template("electrical.html", form=form)
 
 @app.route("/Firmware", methods=['get', 'post'])
@@ -76,7 +76,7 @@ def firmware():
         args = (session['SessionId'],form.firstName.data,form.lastName.data,form.email.data,form.comment.data)
         cursor.execute(query, args).commit()
         flash("Message Received", "success")
-        print("\nData received. ")
+        # print("\nData received. ")
     return render_template("firmware.html", form=form)
 
 @app.route("/About", methods=['get', 'post'])
@@ -87,8 +87,34 @@ def about():
         args = (session['SessionId'],form.firstName.data,form.lastName.data,form.email.data,form.comment.data)
         cursor.execute(query, args).commit()
         flash("Message Received", "success")
-        print("\nData received. ")
+        # print("\nData received. ")
     return render_template("about.html", form=form)
+
+@app.route("/<Folder>/<FileName>", methods=['get'])
+def download(Folder, FileName):
+    safe_path = safe_join(Folder, FileName)
+    print(safe_path)
+    if Folder == 'Mechanical':
+        query = """INSERT INTO Mechanical (SessionId, Part_Name,Part_Path) VALUES(?,?,?)"""
+        args = (session['SessionId'],FileName,safe_path)
+        cursor.execute(query, args).commit()
+        
+    elif Folder == 'Electrical':
+        query = """INSERT INTO Electrical (SessionId, Design_Name,Design_Path) VALUES(?,?,?)"""
+        args = (session['SessionId'],FileName,safe_path)
+        cursor.execute(query, args).commit()
+        
+    elif Folder == 'Firmware':
+        query = """INSERT INTO Firmware (SessionId, File_Name,File_Path) VALUES(?,?,?)"""
+        args = (session['SessionId'],FileName,safe_path)
+        cursor.execute(query, args).commit()
+    else:
+        abort(404)        
+    try:
+        return send_from_directory(app.config["Parts"],safe_path)
+    except FileNotFoundError:
+        abort(404)
+
 
 def create_server_connection():
     conn = None
